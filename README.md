496e4febc69f8261a1b7c7e9cbca471531337c52
Luca <cappelletti.luca94@gmail.com>
2024-11-04 12:21:19 +0100

    Wrote first version of labeler

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# npc-labeler
+# NPC-Labeler
+
 Utility to run the NPC APIs
+
+## Usage
+
+```bash
+python3 labeler.py --input <input_file> --output <output_file>
+```
+
+For instance, suppose you want to classify the SMILES in the metadata of an MGF document and store it into a `classified_matchms.json.gz` file. You can do it by running:
+
+```bash
+python3 labeler.py --input matchms.mgf --output classified_matchms.json.gz
+```
---
+++ labeler.py
@@ -0,0 +1,96 @@
+"""Script to retrieve the classification from the original NP Classifier."""
+
+from typing import Dict, Optional, List
+from argparse import ArgumentParser, Namespace
+import requests
+from tqdm.auto import tqdm
+import compress_json
+from matchms.importing import load_from_mgf
+from cache_decorator import Cache
+from fake_useragent import UserAgent
+from rdkit.Chem.rdchem import Mol
+from rdkit.Chem import MolFromSmiles, MolToSmiles  # pylint: disable=no-name-in-module
+
+
+@Cache(use_source_code=False, cache_path="{cache_dir}/{_hash}.json.gz")
+def get_canonical_smiles_classification(canonical_smiles: str) -> Dict:
+    """Get the classifications for a given SMILES."""
+
+    ua = UserAgent()
+    header = {"User-Agent": str(ua.chrome)}
+
+    response = requests.get(
+        "https://npclassifier.gnps2.org/classify",
+        params={"smiles": canonical_smiles},
+        headers=header,
+        timeout=10,
+    )
+
+    if response.status_code != 200:
+        print(
+            f"Failed to retrieve classifications for {canonical_smiles}, "
+            f" got status code {response.status_code}"
+        )
+        print(response.text)
+
+    try:
+        return response.json()
+    except requests.exceptions.JSONDecodeError:
+        print(
+            f"Failed to convert response to JSON for {canonical_smiles}, "
+            f" got status code {response.status_code}, raw response: {response.text}"
+        )
+
+    return {}
+
+
+def get_smiles_classification(smiles: str) -> Optional[Dict]:
+    """Get the classifications for a given SMILES."""
+    mol: Optional[Mol] = MolFromSmiles(smiles)
+
+    if mol is None:
+        return None
+
+    canonical_smiles: str = MolToSmiles(mol)
+
+    return get_canonical_smiles_classification(canonical_smiles)
+
+
+def labeler() -> None:
+    """Retrieve the classification from the original NP Classifier."""
+    parser = ArgumentParser(
+        description="Retrieve the classification from the original NP Classifier."
+    )
+    parser.add_argument("--input", type=str, help="Path to the input file.")
+    parser.add_argument("--output", type=str, help="Path to the output file.")
+    args: Namespace = parser.parse_args()
+
+    if not any(
+        args.output.endswith(extension)
+        for extension in (".json.gz", ".json", ".json.xz")
+    ):
+        raise ValueError("Only JSON output files are supported.")
+
+    if args.input.endswith(".mgf"):
+        data = (
+            spectrum.metadata["smiles"]
+            for spectrum in load_from_mgf(args.input)
+            if "smiles" in spectrum.metadata
+        )
+    else:
+        raise ValueError("Only MGF files are supported.")
+
+    classifications: List[Dict] = []
+    for smiles in tqdm(
+        data, desc="Retrieving classifications", unit="smiles", dynamic_ncols=True
+    ):
+        classification = get_smiles_classification(smiles)
+
+        if classification is not None:
+            classifications.append(classification)
+
+    compress_json.dump(data, args.output)
+
+
+if __name__ == "__main__":
+    labeler()
